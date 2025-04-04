4018b1a86cb795c7f03de92feaf6548dfcf62c03
gerrydev <gerrydeveloper66@gmail.com>
2025-04-05 03:14:03 +0700

    fix: localStorage is not defined

--- .vitepress/theme/composables/useVersionCheck.ts
+++ .vitepress/theme/composables/useVersionCheck.ts
@@ -6,9 +6,17 @@
  * 2.构建时会生成一个version.json文件，里面包含了当前版本号。
  * 3.在页面加载时，会请求version.json文件，跟当前版本号进行对比。
  * 4.如果远程版本号大于当前版本号，则弹出提示框，询问用户是否刷新页面。
+ * 4.1. 如果用户选择刷新，则刷新页面。
+ * 4.2. 刷新时增加一个标志，避免重复提示。
+ * 4.3. 每次刷新页面时，都会清除提示标志。
  */
 export function useVersionCheck() {
-  localStorage.removeItem("hasPrompted"); // 清除提示标志
+  const isBrowser = typeof window !== "undefined";
+
+  // 只在浏览器环境清除提示标志
+  if (isBrowser) {
+    localStorage.removeItem("hasPrompted");
+  }
 
   const currentVersion = __BUILD_DATE__;
 
